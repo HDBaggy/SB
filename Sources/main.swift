a23115b26e6a84e982821340cee8d30548957025
HarshitDaftary <daftaryharshit@yahoo.co.in>
2017-08-14 10:58:36 +0530

    first commit

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,74 +2,71 @@
 //  main.swift
 //  PerfectTemplate
 //
-//  Created by Kyle Jessup on 2015-11-05.
-//	Copyright (C) 2015 PerfectlySoft, Inc.
-//
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Perfect.org open source project
-//
-// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
-// Licensed under Apache License v2.0
-//
-// See http://perfect.org/licensing.html for license information
-//
-//===----------------------------------------------------------------------===//
-//
 
 import PerfectLib
 import PerfectHTTP
 import PerfectHTTPServer
+import SwiftyGPIO
 
-// An example request handler.
-// This 'handler' function can be referenced directly in the configuration below.
-func handler(data: [String:Any]) throws -> RequestHandler {
-	return {
-		request, response in
-		// Respond with a simple message.
-		response.setHeader(.contentType, value: "text/html")
-		response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
-		// Ensure that response.completed() is called when your processing is done.
-		response.completed()
-	}
+public class GPIOController {
+    
+    let objGpio = SwiftyGPIO.GPIOs(for:.RaspberryPi3)
+    
+    func configurePins(){
+        
+        let objPin = objGpio[.P2]!
+        objPin.direction = .OUT
+        
+        
+        objPin.onRaising{
+            gpio in
+            print("Transition to 1, current value:" + String(gpio.value))
+        }
+        
+        objPin.onFalling{
+            gpio in
+            print("Transition to 0, current value:" + String(gpio.value))
+        }
+        
+        objPin.onChange{
+            gpio in
+            gpio.clearListeners()
+            print("The value changed, current value:" + String(gpio.value))
+        }
+        
+        
+    }
+    
 }
 
-// Configuration data for an example server.
-// This example configuration shows how to launch a server
-// using a configuration dictionary.
 
+let objGpioController = GPIOController()
+let objServer = HTTPServer()
+objServer.serverPort = 8181
 
-let confData = [
-	"servers": [
-		// Configuration data for one server which:
-		//	* Serves the hello world message at <host>:<port>/
-		//	* Serves static files out of the "./webroot"
-		//		directory (which must be located in the current working directory).
-		//	* Performs content compression on outgoing data when appropriate.
-		[
-			"name":"localhost",
-			"port":8181,
-			"routes":[
-				["method":"get", "uri":"/", "handler":handler],
-				["method":"get", "uri":"/**", "handler":PerfectHTTPServer.HTTPHandler.staticFiles,
-				 "documentRoot":"./webroot",
-				 "allowResponseFilters":true]
-			],
-			"filters":[
-				[
-				"type":"response",
-				"priority":"high",
-				"name":PerfectHTTPServer.HTTPFilter.contentCompression,
-				]
-			]
-		]
-	]
-]
+var objRoutes = Routes()
+objRoutes.add(method: .get, uri: "/") { (request, response) in
+    
+    response.setHeader(.contentType, value: "text/html")
+    response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>").completed()
+}
+
+objRoutes.add(method: .get, uri: "/gpio") { (request, response) in
+    
+    
+    objGpioController.configurePins()
+    
+    response.setHeader(.contentType, value: "text/html")
+    response.appendBody(string: "<html><title>Hello, world!</title><body>Welcome to GPIO page</body></html>").completed()
+}
+
+objServer.addRoutes(objRoutes)
 
 do {
-	// Launch the servers based on the configuration data.
-	try HTTPServer.launch(configurationData: confData)
+    try objServer.start()
 } catch {
-	fatalError("\(error)") // fatal error launching one of the servers
+    
+    print("There is an error starting the server")
 }
+
 
